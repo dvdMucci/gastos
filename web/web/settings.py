6ff088e836253c91a4732ca2afbcfb17aaba8212
David Guillermo Mucci <dvd.mucci@gmail.com>
2025-10-07 23:24:00 +0000

    Add 'income' and 'security' apps to INSTALLED_APPS and verify all required apps are present

--- web/web/settings.py
+++ web/web/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'finances',
     'subscriptions',
     'forecasts',
+    'income',
+    'security',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
